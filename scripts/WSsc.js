4bb3bd79de2d2c5ff9ace5001c765118c46e5e48
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-09-19 14:53:54 +0930

    Finished Arithmatic Worksheet

--- scripts/WSsc.js
+++ scripts/WSsc.js
@@ -7,12 +7,11 @@ var questions = [
     document.getElementById('Q5'), 
 ];
 function generateQuestions(){
-    console.log('Function Run')
+
     for (let i = 0; i < questions.length; i++) {
         let valueA = Math.floor(Math.random() * 100); //Generates a number between 1 & 100
         let valueB = Math.floor(Math.random() * 100); //Generates a number between 1 & 100
         let operation = ops[Math.floor(Math.random()*ops.length)]
-        questions[i].textContent = ('Question '+(i+1)+': '+valueA+operation+valueB+'=')
+        questions[i].innerHTML = ('<b>Question '+(i+1)+': </b>'+valueA+operation+valueB+'=')
     }
 }
-
