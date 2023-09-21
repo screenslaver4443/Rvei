395d24b1404590a98d0efe0b50171704e1bc6b46
BritishFrogMan <144877994+BritishFrogMan@users.noreply.github.com>
2023-09-21 10:25:27 +0930

    added multiple cards to see if copying works

--- scripts/FCsc.js
+++ scripts/FCsc.js
@@ -1,5 +1,29 @@
-const card = document.querySelector('.card__inner');
+const cardOne = document.querySelector('.card__inner__One');
+const cardTwo = document.querySelector('.card__inner__Two');
+const cardThree = document.querySelector('.card__inner__Three');
+const cardFour = document.querySelector('.card__inner__Four');
+const cardFive = document.querySelector('.card__inner__Five');
+const cardSix = document.querySelector('.card__inner__Six');
+const cardSeven = document.querySelector('.card__inner__Seven');
 
-function cardFlip() {
-    card.classList.toggle('is-flipped');
+function cardFlip__One() {
+    cardOne.classList.toggle('is-flipped');
+}
+function cardFlip__Two() {
+    cardTwo.classList.toggle('is-flipped');
+}
+function cardFlip__Three() {
+    cardThree.classList.toggle('is-flipped');
+}
+function cardFlip__Four() {
+    cardFour.classList.toggle('is-flipped');
+}
+function cardFlip__Five() {
+    cardFive.classList.toggle('is-flipped');
+}
+function cardFlip__Six() {
+    cardSix.classList.toggle('is-flipped');
+}
+function cardFlip__Seven() {
+    cardSeven.classList.toggle('is-flipped');
 }
