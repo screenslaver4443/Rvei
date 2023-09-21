c20d753c95d318cc5df26bd16b7aed00a5b33a0b
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-09-21 10:26:24 +0930

    Partically wrote Answer generation

--- scripts/WSsc.js
+++ scripts/WSsc.js
@@ -6,12 +6,29 @@ var questions = [
     document.getElementById('Q4'), 
     document.getElementById('Q5'), 
 ];
-function generateQuestions(){
+var answers = []; //array answers
+function generateQuestionsArithmatic(){
 
     for (let i = 0; i < questions.length; i++) {
         let valueA = Math.floor(Math.random() * 100); //Generates a number between 1 & 100
         let valueB = Math.floor(Math.random() * 100); //Generates a number between 1 & 100
-        let operation = ops[Math.floor(Math.random()*ops.length)]
-        questions[i].innerHTML = ('<b>Question '+(i+1)+': </b>'+valueA+operation+valueB+'=')
+        let operation = ops[Math.floor(Math.random()*ops.length)];
+        questions[i].innerHTML = ('<b>Question '+(i+1)+': </b>'+valueA+operation+valueB+'=');
+    }
+}
+function generateQuestionsSurds(){
+    for (let i = 0; i < questions.length; i++) {
+        let plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueAb = Math.floor(Math.random() * 25+1)*plusOrMinus; //Generates a number between 1 & 100
+        //let valueBb = Math.floor(Math.random() * 25); //Generates a number between 1 & 100
+        plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueAp = Math.floor(Math.random() * 5)*plusOrMinus; //Generates a number between 1 & 100
+        plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueBp = Math.floor(Math.random() * 5)*plusOrMinus; //Generates a number between 1 & 100
+        let operation = ops[Math.floor(Math.random()*ops.length)];
+        questions[i].innerHTML = ('<b>Question '+(i+1)+': </b>'+`${valueAb}<sup>${valueAp}</sup>${operation}${valueAb}<sup>${valueBp}</sup>=`);
+        // ToDo: Round to 2 decimel Places
+        answers.push(eval(`${Math.pow(valueAb, valueAp)} ${operation} ${Math.pow(valueAb, valueBp)}`))
+        
     }
 }
