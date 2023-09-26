9145ae302f8179d0a960ade810920c8987282239
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2023-09-26 11:46:44 +0930

    Fixed buttons and added multiple cards.

--- scripts/FCsc.js
+++ scripts/FCsc.js
@@ -1,38 +1,30 @@
 const cardOne = document.querySelector('.card__inner__One');
-const cardTwo = document.querySelector('.card__inner__Two');
-const cardThree = document.querySelector('.card__inner__Three');
-const cardFour = document.querySelector('.card__inner__Four');
-const cardFive = document.querySelector('.card__inner__Five');
-const cardSix = document.querySelector('.card__inner__Six');
-const cardSeven = document.querySelector('.card__inner__Seven');
-
-const Two = document.querySelector('.two');
+const cardHeaders = ['First Index Law', 'Second Index Law', 'Third Index Law', 'Fourth Index Law', 'Fifth Index Law', 'Sixth Index Law', 'Seventh Index Law'];
+const cardBodies = ['<img src="" alt="" class="ind1">', '<p>test</p>']; //todo: Fix this shit
+const cardHeadFrontHtml = document.getElementById('cardHeaderF');
+const cardHeadBackHtml = document.getElementById('cardHeaderB');
+const cardBodyBackHtml = document.getElementById('cardBody');
+var cardNumber = 0 
 
 function cardFlip__One() {
     cardOne.classList.toggle('is-flipped');
 }
-function cardFlip__Two() {
-    cardTwo.classList.toggle('is-flipped');
-}
-function cardFlip__Three() {
-    cardThree.classList.toggle('is-flipped');
-}
-function cardFlip__Four() {
-    cardFour.classList.toggle('is-flipped');
-}
-function cardFlip__Five() {
-    cardFive.classList.toggle('is-flipped');
-}
-function cardFlip__Six() {
-    cardSix.classList.toggle('is-flipped');
-}
-function cardFlip__Seven() {
-    cardSeven.classList.toggle('is-flipped');
+
+function updateCard(){
+    cardHeadFrontHtml.innerHTML = cardHeaders[cardNumber];
+    cardHeadBackHtml.innerHTML = cardHeaders[cardNumber];
+    cardBodyBackHtml.innerHTML = cardBodies[cardNumber];
 }
 
-function changeCardLeft(twoID) {
-    Two.classList.toggle('two');
+function changeCardLeft() {
+    if (cardNumber >0){
+        cardNumber--;
+    }
+    updateCard();
 }
 function changeCardRight() {
-    
+    if (cardNumber <8){
+        cardNumber++;
+    }
+    updateCard();
 }
