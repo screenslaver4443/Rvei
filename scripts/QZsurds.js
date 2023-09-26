1742fae40586c10ce4b17921c46b6751fb4fc0de
BritishFrogMan <144877994+BritishFrogMan@users.noreply.github.com>
2023-09-26 13:58:36 +0930

    started designing the quiz section

---
+++ scripts/QZsurds.js
@@ -0,0 +1,31 @@
+var ops = ['+', '-', '*', '/'];
+var questions = [
+    document.getElementById('question')
+];
+var answers = [];
+var q = 0;
+
+function reGenerateQuestionsSurds() {
+    q += 1;
+    for (let i = 0; i < questions.length; i++) {
+        let plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueAb = Math.floor(Math.random() * 10+1)*plusOrMinus; //Generates a number between 1 & 100
+        //let valueBb = Math.floor(Math.random() * 25); //Generates a number between 1 & 100
+        plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueAp = Math.floor(Math.random() * 5)*plusOrMinus; //Generates a number between 1 & 100
+        plusOrMinus = Math.random() < 0.5 ? -1 : 1; //Chooses if the next number is negative or positive
+        let valueBp = Math.floor(Math.random() * 5)*plusOrMinus; //Generates a number between 1 & 100
+        let operation = ops[Math.floor(Math.random()*ops.length)];
+        questions[i].innerHTML = ('<b>Question '+(i+q)+': </b>'+`${valueAb}<sup>${valueAp}</sup> ${operation} (${valueAb}<sup>${valueBp}</sup>)`);
+        answers.push(Math.round(eval(`${Math.pow(valueAb, valueAp)} ${operation} ${Math.pow(valueAb, valueBp)}`)*100)/100);
+    }
+}
+
+function viewAnswers(){
+    let answershtml = ''; //Place holder for the html that the loop will append to. This is because using innerHTML in the loop will not work properly for making a list.
+    alert("Answers are at bottom of the page, reload to remove them, click view answers when questions are refreshed to update the answers.")
+    for (let i = 0; i < answers.length; i++) {
+        answershtml += `<li>${answers[i]}</li>`;
+    }
+    document.getElementById('answers').innerHTML = '<h2>Answers:</h2><ol>'+answershtml+'</ol>';
+}
